2c846ed6caa35bf3715ee346622bb43cc6e3e0fb
Trinity Balderas <trinitybalderas02@gmail.com>
2022-09-27 21:42:26 -0500

    inital commit

--- src/components/detailComponents/DetailScreen.jsx
+++ src/components/detailComponents/DetailScreen.jsx
@@ -1,64 +1,11 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import axios from 'axios'
-import { url } from '../homeComponents/HomeScreen'
+import React from 'react'
 
-const DetailScreen = () => {
-	const { id } = useParams()
-	const [recipe, setRecipe] = useState({})
+const DetailScreen = () => {  
+  return (
+    <section>
+      {/* Welcome to the details page! This page will be reusable. Follow instructions to know what to do here. */}
+    </section>
+  );
+};
 
-	useEffect(() => {
-		axios.get(`${url}/recipes/${id}`).then(res => {
-			setRecipe(res.data)
-			console.log(res.data)
-		})
-	}, [id])
-
-	return (
-		<div className="detail">
-			<div
-				className="details-banner"
-				style={{ backgroundImage: `url(${recipe.image_url})` }}
-			>
-				{' '}
-				<div className="overlay"></div>
-				<h1 className="food-title">{recipe.recipe_name}</h1>
-			</div>
-			<section className="recipe-details-section">
-				<div className="recipe-card details">
-					<h2 className="recipe-header">Recipe</h2>
-					<p className="prep-time">Prep Time: {recipe.prep_time}</p>
-					<p className="cook-time">Cook Time: {recipe.cook_time}</p>
-					<p className="serves">Serves: {recipe.serves}</p>
-					<h2 className="ingredients-header">Ingredients</h2>
-					{recipe.ingredients ? (
-						recipe.ingredients.map(ingredient => {
-							return (
-								<p
-									className="ingredients"
-									key={recipe.ingredients.ingredient_id}
-								>
-									{ingredient.quantity}{' '}
-									{ingredient.ingredient}
-								</p>
-							)
-						})
-					) : (
-						<p>No ingredients</p>
-					)}
-				</div>
-				<div className="recipe-card instructions">
-					<h2 className="instructions-title">Instructions</h2>
-					<article className="instructions-text">
-						<p style={{ whiteSpace: 'pre-wrap' }}>
-							{recipe.instructions &&
-								JSON.parse(JSON.stringify(recipe.instructions))}
-						</p>
-					</article>
-				</div>
-			</section>
-		</div>
-	)
-}
-
-export default DetailScreen
+export default DetailScreen;
--- src/components/homeComponents/HomeScreen.jsx
+++ src/components/homeComponents/HomeScreen.jsx
@@ -1,56 +1,13 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-
-import mag from '../../assets/burger.jfif'
+import React from 'react'
 import AdBanner from './AdBanner'
-import RecipeCard from '../newRecipeComponents/RecipeCard'
 
-export const url = 'https://recipes.devmountain.com'
-
-const HomeScreen = () => {
-	const [recipes, setRecipes] = useState([])
-	const [search, setSearch] = useState('')
-
-	const getRecipes = () => {
-		axios.get(`${url}/recipes`).then(res => {
-			setRecipes(res.data)
-		})
-	}
-
-	let filteredRecipes = recipes.filter(
-		recipe =>
-			recipe.recipe_name.toLowerCase().includes(search.toLowerCase()) &&
-			recipe.recipe_name !== 'undefined'
-	)
-
-	useEffect(() => {
-		getRecipes()
-	}, [])
-
-	return (
-		<div className="container">
-			<AdBanner />
-			<div className="search-wrapper">
-				<input
-					className="search-input"
-					type="text"
-					placeholder="Search for a Recipe"
-					value={search}
-					onChange={e => setSearch(e.target.value)}
-				/>
-				<img className="mag" src={mag} alt="magnifying glass" />
-			</div>
-			<main className="recipe-cards-container">
-				{filteredRecipes.length > 0 ? (
-					filteredRecipes.map(recipe => (
-						<RecipeCard recipe={recipe} key={recipe.recipe_id} />
-					))
-				) : (
-					<p>There are no recipes with that name.</p>
-				)}
-			</main>
-		</div>
-	)
+const HomeScreen = () => {  
+  return (
+    <div>
+      <AdBanner />
+      {/* Much code from Part 2 will be placed around here. Do your best! */}
+    </div>
+  )
 }
 
 export default HomeScreen
