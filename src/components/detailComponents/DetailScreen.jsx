882aa70e4b25f8d4d9a22f5f17afca7b259c3cb8
Trinity Balderas <trinitybalderas02@gmail.com>
2022-09-28 09:48:53 -0500

    css

--- src/components/detailComponents/DetailScreen.jsx
+++ src/components/detailComponents/DetailScreen.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import ImgDetail from "./ImgDetail";
 
-const DetailScreen = () => {  
+const DetailScreen = () => {
+  const { id } = useParams();
+  const [recipe, setRecipe] = useState({});
+  console.log(recipe);
+
+  const url = `https://recipes.devmountain.com`;
+
+  useEffect(() => {
+    axios.get(`${url}/recipes/${id}`).then((response) => {
+      setRecipe(response.data);
+    });
+  }, []);
+
   return (
-    <section>
-      {/* Welcome to the details page! This page will be reusable. Follow instructions to know what to do here. */}
-    </section>
+    <div>
+      <ImgDetail className="imgDetail" title={recipe.recipe_name} image={recipe.image_url}/>
+      <div className="detailsContainer">
+      <div className="ingredients">
+        <h2>Recipe</h2>
+        <h4>Prep Time: {recipe.prep_time}</h4>
+        <h4>Cook Time: {recipe.cook_time}</h4>
+        <h4>Serves: {recipe.serves}</h4>
+        <h2>Ingredients</h2>
+        {recipe.ingredients &&
+          recipe.ingredients.map((ing, index) => {
+            return (
+              <h4>
+                {ing.quantity} {ing.ingredient}
+              </h4>
+            );
+          })}
+      </div>
+      <div className="instructions">
+        <h2>Instructions</h2>
+        <p style={{ whiteSpace: "pre-wrap" }}>
+          {recipe.instructions && JSON.parse(recipe.instructions)}
+        </p>
+      </div>
+    </div>
+    </div>
   );
 };
 
