c7c150c753f9759fc2676453c84a46286af7d901
Trinity Balderas <trinitybalderas02@gmail.com>
2022-09-28 02:18:25 -0500

    Inital commit

--- src/components/homeComponents/HomeScreen.jsx
+++ src/components/homeComponents/HomeScreen.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import AdBanner from './AdBanner'
-//import RecipeCard from '../newRecipeComponents/RecipeCard'
+import RecipeCard from '../newRecipeComponents/RecipeCard'
 
 export const url = 'https://recipes.devmountain.com'
 
 const HomeScreen = () => {  
-  const [setRecipes] = useState([])
-	//const [search, setSearch] = useState('')
+  const [recipes, setRecipes] = useState([])
+	const [search, setSearch] = useState('')
   const getRecipes = () => {
     axios
         .get(`${url}/recipes`)
@@ -16,16 +16,35 @@ const HomeScreen = () => {
             console.log(res.data)
         })
 }
+const searchedRecipe = recipes
+    .filter((recipe, index) => {
+      let title = recipe.recipe_name.toLowerCase();
+      let searchParams = search.toLowerCase();
+      return title.includes(searchParams);
+    })
+    .map((recipe, index) => {
+      return <RecipeCard recipe={recipe} />;
+    });
 
   useEffect(() => {
       getRecipes()
   },[])
+
   return (
     <div>
-      <AdBanner />
-      {/* Much code from Part 2 will be placed around here. Do your best! */}
+      <AdBanner classname="banner" />
+      <div className="homeSearch">
+        <input
+          className="searchBar"
+          type="text"
+          value={search}
+          onChange={(event) => setSearch(event.target.value)}
+          placeholder="Search for a Recipe"
+        />
+      </div>
+      <div className="srchRecipe">{searchedRecipe}</div>
     </div>
-  )
+  );
 }
 
 export default HomeScreen
